62c69d50c1bca5d2520fe8baf0a8ec8d51cf4b44
Ignacio Baptista [PC] <ibapista@frba.utn.edu.ar>
2021-07-31 20:26:16 -0300

    Agregado bucle

--- 3xPlus1/main.cpp
+++ 3xPlus1/main.cpp
@@ -2,22 +2,27 @@
 using namespace std;
 
 int main() {
-    int number, iteracion = 0;
+    while(true){
+        int number, iteracion = 0;
 
-    cout<<"Inserte numero inicial: "<<endl;
-    cin>>number;
+        cout << "Inserte numero inicial: " << endl;
+        cin >> number;
 
-    while (number != 1){
-
-        cout<<iteracion<<" | Siguiente numero: "<<number<<endl;
-
-        if (number % 2 == 0){
-            number /= 2;
-        } else {
-            number = number * 3 + 1;
+        if (number == -1){
+            return 0;
         }
 
-        iteracion++;
+        while (number != 1) {
+
+            cout << iteracion << " | Siguiente numero: " << number << endl;
+
+            if (number % 2 == 0) {
+                number /= 2;
+            } else {
+                number = number * 3 + 1;
+            }
+
+            iteracion++;
+        }
     }
-    return 0;
 }
